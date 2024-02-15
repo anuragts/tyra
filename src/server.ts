01bf98d9f7ac42e6f7741c596805401597513fa2
Anurag30112003 <anuragsharma011011@gmail.com>
2024-02-15 10:25:08 +0530

    feat: default html file

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,20 @@
 import { createServer } from "http";
+import { readFileSync } from "fs";
+import { resolve } from "path"  ;
 
-export default function server(file?: string, port?: number) {
+export default function server(filename?: string, port?: number) {
   createServer((request, response) => {
+    if(!filename) {
+        response.writeHead(200, { "Content-Type": "text/html" });
+        const fileContents = readFileSync(resolve(__dirname, 'static/tyra.html'), 'utf-8');
+        response.write(fileContents);
+        response.end();
+        return;
+      }
+  
     const contentType = "text/plain";
     response.writeHead(200, { "Content-Type": contentType });
-    response.write(`Hello World from ${file}\n`);
+    response.write(`Hello World from ${filename}\n`);
     response.end();
   }).listen(port || 3000);
 
