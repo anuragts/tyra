b7536f30d0662b0c54b4ffb57f0a542bed247d7f
Anurag30112003 <anuragsharma011011@gmail.com>
2024-02-15 10:48:03 +0530

    feat: add check for 404 error

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,37 @@
 import { createServer } from "http";
 import { readFileSync } from "fs";
-import { resolve } from "path"  ;
+import { resolve } from "path";
 
 export default function server(filename?: string, port?: number) {
   createServer((request, response) => {
-    if(!filename) {
-        response.writeHead(200, { "Content-Type": "text/html" });
-        const fileContents = readFileSync(resolve(__dirname, 'static/tyra.html'), 'utf-8');
-        response.write(fileContents);
-        response.end();
-        return;
-      }
-  
-    const contentType = "text/plain";
-    response.writeHead(200, { "Content-Type": contentType });
-    response.write(`Hello World from ${filename}\n`);
+    let fileContents;
+    if (!filename) {
+      response.writeHead(200, { "Content-Type": "text/html" });
+      fileContents = readFileSync(
+        resolve(__dirname, "static/tyra.html"),
+        "utf-8"
+      );
+      response.write(fileContents);
+      response.end();
+      return;
+    }
+
+    try {
+      fileContents = readFileSync(resolve(__dirname, filename), "utf-8");
+    } catch (e) {
+      response.writeHead(404, { "Content-Type": "text/html" });
+      fileContents = readFileSync(
+        resolve(__dirname, "static/404.html"),
+        "utf-8"
+      );
+      response.write(fileContents);
+      console.error(e);
+      response.end();
+      return;
+    }
+
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write(fileContents);
     response.end();
   }).listen(port || 3000);
 
